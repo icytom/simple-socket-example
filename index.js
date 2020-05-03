2c9e36aca1de69cb67d72ee62c030013082b6583
Tom <tomgarne@gmail.com>
2020-05-03 20:37:31 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ let app = require('express')();
 let http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
+
+const PORT = process.env.PORT || 3000;
+
 app.get ('/', (req,res) => {
     res.sendFile(__dirname + '/index.html');
 })
@@ -10,6 +13,6 @@ io.on('connection', (socket) => {
     console.log('a user connected');
 })
 
-http.listen(process.env.PORT || 3000, () => {
+http.listen(PORT, () => {
     console.log('listening on *:3000');
 })
