a05ed067728f07a36740e09ec04a9b6beb7733ec
Tom <tomgarne@gmail.com>
2020-05-03 20:35:01 +0100

    added env.port

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ io.on('connection', (socket) => {
     console.log('a user connected');
 })
 
-http.listen(3000, () => {
+http.listen(process.env.PORT || 3000, () => {
     console.log('listening on *:3000');
 })
