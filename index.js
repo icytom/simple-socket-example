c258dae8f15600109152c7a341cae5ea77e60c79
Tom <tomgarne@gmail.com>
2020-05-03 20:39:10 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ const PORT = process.env.PORT || 3000;
 
 app.get ('/', (req,res) => {
     res.sendFile(__dirname + '/index.html');
-})
+});
 
 io.on('connection', (socket) => {
     console.log('a user connected');
-})
+});
 
 http.listen(PORT, () => {
     console.log('listening on *:3000');
-})
+});
